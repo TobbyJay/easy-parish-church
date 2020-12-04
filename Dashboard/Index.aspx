f28fb58a04247171192621c5bd7c71d11d68f639
Tobby Umoh <tobbyumoh@gmail.com>
2020-12-04 11:18:53 +0100

    add ll webform files

---
+++ Dashboard/Index.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="Dashboard" Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master"  CodeBehind="Index.aspx.cs" Inherits="EasyParish.Dashboard.Index" %>
+
+
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+        <div>
+            <h1 class="">Hello There this is the dashboard</h1>
+        </div>
+
+</asp:Content>
---
+++ Login.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EasyParish
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (username.Text == "admin" && password.Text == "admin")
+            {
+                Response.Redirect("~/Dashboard/Index");
+               
+            }
+            else
+            {
+                Label1.Text = "Sorry Invalid Password try again";
+            }
+           
+        }
+    }
+}
